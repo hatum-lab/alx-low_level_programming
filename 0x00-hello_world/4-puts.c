89d7483df12c1073d745baf4eebb979a537eff83
Hatim AB <hatimaboutaleb.98@gmail.com>
2023-02-19 10:34:37 -0800

    hihlhuhjiji

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
-*/ program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line /*
-int main(){
-	puts(""Programming is like building a multilingual puzzle\n");
-	return 0 ;
+/*
+* program that prints
+* exactly"Programming is 
+* like building a multilingual
+* puzzle,followed by a new line 
+*/
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle\n )
+return(0);
 }
-	
