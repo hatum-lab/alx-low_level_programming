9ed43e8fd16cc45be393e4847f07b5682e3adeac
Hatim AB <hatimaboutaleb.98@gmail.com>
2023-02-20 04:49:12 -0800

    jaieiinji

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - print a line using printf founction
- * Return 0 (Success)
+ * Return: 0 (Success)
 */
-int main(void) {
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
