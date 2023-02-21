c47a676675fd77177324496f117377018dfd2978
Hatim AB <hatimaboutaleb.98@gmail.com>
2023-02-21 15:50:14 -0800

    lojih

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,22 @@
 #include "main.h"
-#include "0-putchar.c"
-/**
-*main - program that prints _putchar followed by a new line
-* Return: (0)success
-*/
-int main(void) {
-    char c = '_';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'p';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'u';
-    write(STDOUT_FILENO, &c, 1);
-    c = 't';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'c';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'h';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'a';
-    write(STDOUT_FILENO, &c, 1);
-    c = 'r';
-    write(STDOUT_FILENO, &c, 1);
-    c = '\n';
-    write(STDOUT_FILENO, &c, 1);
-    return 0;
-}
+	/**
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
+
+
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
 
