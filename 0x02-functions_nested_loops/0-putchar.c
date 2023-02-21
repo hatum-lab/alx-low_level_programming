634f12bce9dda1f428a3dd4a6eb71f00fd440f03
Hatim AB <hatimaboutaleb.98@gmail.com>
2023-02-21 11:55:54 -0800

    çhuhuhoippok

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,27 @@
-#include <_putchar.h>
-#include <main.h>
+#include "main.h"
 /**
 *main - program that prints _putchar followed by a new line
 * Return: (0)success
 */
-int main(void)
-{
-char str[] = ("_putchar\n");
-int i = 0;
-while (str[i] != '\0')
-{
-char c = str[i];
+int main(void) {
+    char c = '_';
+    write(STDOUT_FILENO, &c, 1);
+    c = 'p';
+    write(STDOUT_FILENO, &c, 1);
+    c = 'u';
+    write(STDOUT_FILENO, &c, 1);
+    c = 't';
+    write(STDOUT_FILENO, &c, 1);
+    c = 'c';
+    write(STDOUT_FILENO, &c, 1);
+    c = 'h';
+    write(STDOUT_FILENO, &c, 1);
+    c = 'a';
+    write(STDOUT_FILENO, &c, 1);
+    c = 'r';
+    write(STDOUT_FILENO, &c, 1);
+    c = '\n';
+    write(STDOUT_FILENO, &c, 1);
+    return 0;
+}
 
-_putchar (c);
-i++;
-}
-return (0);
-}
