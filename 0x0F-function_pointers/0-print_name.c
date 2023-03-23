c5be624cf7a10b9f30c8fc4ca95476d50df3ceda
hatim <my@email.com>
2023-03-22 18:36:06 -0700

    hihoi

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name as is
  * @name: name of the person
  * @f: is pointer
 **/
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
